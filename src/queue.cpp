723745dab881c28fa47bae22346c2eb3510a3a9b
lm-olano <lm_olano@hotmail.com>
2020-05-04 12:31:42 +0200

    Added several method to the queue class

--- src/queue.cpp
+++ src/queue.cpp
@@ -1,6 +1,76 @@
 #include "queue.hpp"
 
-Queue::Queue() {
+#include <iostream>
+
+Queue::Queue(int max_size) {
+    head = nullptr;
+    tail = nullptr;
+    _max_size = max_size;
+    size = 0;
+}
 
 
+bool Queue::is_empty() {
+    return size <= 0 ? true : false;
+}
+
+
+bool Queue::has_space() {
+    return size < _max_size ? true : false;
+}
+
+
+int Queue::peek() {
+    if (!is_empty()) {
+        return (*head).get_value();
+    }
+    else {
+        std::cout << "The queue is empty." << std::endl;
+    }
+}
+
+
+void Queue::enqueue(int new_value) {
+
+    if (has_space()) {
+        Node* new_node = new Node(new_value);
+        if (is_empty()) {
+            
+            head = new_node;
+            tail = new_node;
+        }
+        else {
+
+            (*tail).set_next(new_node);
+
+            tail = new_node;
+        }
+        size += 1;
+    }
+    else {
+        std::cout << "The queue is full." << std::endl;
+    }
+}
+
+
+int Queue::dequeue() {
+    if (!is_empty()) {
+        Node* temp = head; 
+        int value;
+        if (size == 1) {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else {
+            head = (*head).get_next();
+        }
+        value = (*temp).get_value();
+        delete temp;
+        size -= 1;
+
+        return value;
+    }
+    else {
+        std::cout << "The queue is empty." << std::endl;
+    }
 }
--- src/queue.hpp
+++ src/queue.hpp
@@ -5,7 +5,19 @@ class Queue {
         Node* head;
         Node* tail;
 
+        int size;
+        int _max_size;
     public:
-        Queue();
+        Queue(int max_size = 1000);
 
+        Node* get_head() {return head;}
+        int get_size() {return size;}
+
+        bool is_empty();
+        bool has_space();
+
+        int peek();
+
+        void enqueue(int new_value);
+        int dequeue();
 };
