62f67f0fe63c31ebb7918c208c441cfd1e29aa70
lm-olano <lm_olano@hotmail.com>
2020-05-05 16:22:38 +0200

    Reworked linked list to accept any type of data.

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -1,25 +1,14 @@
 #include "linked_list.hpp"
 
 #include <iostream>
-
-// Class constructors
-Node::Node(int value, Node* next) {
-
-    _value = value;
-    _next = next;
-}
-
-LinkedList::LinkedList(int value) {
-    head = new Node(value);
-}
+#include <sstream>
 
 
 // Linked list methods
-void LinkedList::insert_beginning(int new_value) {
+template <class T>
+void LinkedList<T>::insert_beginning(T new_value) {
     
-    Node* new_node = new Node(new_value);
-
-    std::cout << "Adding node with value " << std::to_string(new_value) << " at " << new_node << std::endl;
+    Node<T>* new_node = new Node<T>(new_value);
 
     new_node->set_next(head);
 
@@ -27,59 +16,58 @@ void LinkedList::insert_beginning(int new_value) {
 
 }
 
-void LinkedList::insert(int new_value, int previous_value) {
+template <class T>
+void LinkedList<T>::insert(T new_value, T previous_value) {
 
-    Node* previous_node;
-    Node* next_node;
+    Node<T>* previous_node;
+    Node<T>* next_node;
 
     previous_node = search_node(previous_value);
 
     if (previous_node != nullptr) {
 
-        Node* new_node = new Node(new_value);
+        Node<T>* new_node = new Node<T>(new_value);
 
-        previous_node = (*previous_node).get_next();
+        previous_node = previous_node->get_next();
         
-        next_node = (*previous_node).get_next();
-        (*previous_node).set_next(new_node);
-        (*new_node).set_next(next_node);
+        next_node = previous_node->get_next();
+        previous_node->set_next(new_node);
+        new_node->set_next(next_node);
     }
     else {
         std::cout << "Value " << previous_value << " is not in list." << std::endl;
     }
-
-
 }
 
-void LinkedList::remove_node(int value_to_remove) {
+template <class T>
+void LinkedList<T>::remove_node(T value_to_remove) {
 
-    Node* prev_node;
-    Node* rem_node;
+    Node<T>* prev_node;
+    Node<T>* rem_node;
 
     prev_node = search_node(value_to_remove);
     
 
     if (prev_node != nullptr) {
-        rem_node = (*prev_node).get_next();
+        rem_node = prev_node->get_next();
 
-        std::cout << "Removing node with value " << std::to_string(value_to_remove) << " at " << rem_node << std::endl;
-
-        (*prev_node).set_next((*rem_node).get_next());
+        prev_node->set_next(rem_node->get_next());
 
         delete rem_node;
     }
-
 }
 
-Node* LinkedList::search_node(int value) {
+
+template <class T>
+Node<T>* LinkedList<T>::search_node(T value) {
     // The method returns the a pointer to the node BEFORE the node which holds the value
 
-    Node* current_node = head;
-    Node* next_node = (*current_node).get_next();
+    Node<T>* current_node = head;
+    Node<T>* next_node = current_node->get_next();
 
-    while ((next_node != nullptr) && ((*next_node).get_value() != value)) {
-        current_node = (*current_node).get_next();
-        next_node = (*next_node).get_next();
+    while ((next_node != nullptr) && (next_node->get_value() != value)) {
+        current_node = current_node->get_next();
+        next_node = next_node->get_next();
     }
 
     if (next_node == nullptr) {
@@ -92,29 +80,47 @@ Node* LinkedList::search_node(int value) {
     return current_node;
 }
 
-std::string LinkedList::stringify() {
-    Node* current_node = head;
-    std::string list_string = "";
+template <class T>
+std::string LinkedList<T>::stringify() {
+    Node<T>* current_node = head;
+    std::ostringstream buffer;
 
     while (current_node != nullptr) {
-        list_string += std::to_string((*current_node).get_value());
-        list_string += " ";
+        buffer << current_node->get_value();
+        buffer << " ";
 
-        current_node = (*current_node).get_next();
+        current_node = current_node->get_next();
     }
-    return list_string;
+    return buffer.str();
 }
 
-void LinkedList::remove_all() {
-    Node* current_node = head;
-    Node* temp;
+
+template<class T>
+void LinkedList<T>::remove_all() {
+    Node<T>* current_node = head;
+    Node<T>* temp;
     
     while (current_node != nullptr) {
         temp = current_node;
-        current_node = (*current_node).get_next();
-        std::cout << "Removing node with value " << std::to_string((*temp).get_value()) << " at " << temp << std::endl;
+        current_node = current_node->get_next();
         delete temp;
     }
 
     head = nullptr;
 };
+
+
+// explicit instantiations
+template class Node<int>;
+template class Node<int*>;
+template class Node<float>;
+template class Node<double>;
+template class Node<bool>;
+template class Node<std::string>;
+
+template class LinkedList<int>;
+template class LinkedList<int*>;
+template class LinkedList<float>;
+template class LinkedList<double>;
+template class LinkedList<bool>;
+template class LinkedList<std::string>;
--- src/linked_list.hpp
+++ src/linked_list.hpp
@@ -3,39 +3,43 @@
 
 #include <string>
 
+template <class T>
 class Node {
     private:
-        int _value;
-        Node* _next;
+        T _value;
+        Node<T>* _next;
 
     public:
+        Node(): _next(nullptr), _value(0) {}
+        Node(T value) : _next(nullptr), _value(value) {}
+        Node(Node<T>* node) : _value(node->get_value()), _next(node->get_next()) {}
 
-        Node(int value, Node* next=nullptr);
+        T get_value() {return _value;}
+        Node<T>* get_next() {return _next;}
 
-        int get_value() {return _value;}
-        Node* get_next() {return _next;}
-
-        void set_value(int value) {_value = value;}
-        void set_next(Node* next) {_next = next;}
+        void set_value(T value) {_value = value;}
+        void set_next(Node<T>* next) {_next = next;}
 
 };
 
+template <class T>
 class LinkedList {
     private:
-        Node* head;
-        Node* tail;
+        Node<T>* head;
+        Node<T>* tail;
 
     public:
-        LinkedList(int value = 0);
+        LinkedList() {head = nullptr;}
+        LinkedList(T value) {head = new Node<T>(value);};
 
-        Node* get_head_node() {return head;}
-        Node* get_tail_node() {return tail;}
+        Node<T>* get_head_node() {return head;}
+        Node<T>* get_tail_node() {return tail;}
 
-        void insert_beginning(int new_value);
-        void insert(int new_value, int previous_value);
-        void remove_node(int value_to_delete);
+        void insert_beginning(T new_value);
+        void insert(T new_value, T previous_value);
+        void remove_node(T value_to_delete);
 
-        Node* search_node(int value);
+        Node<T>* search_node(T value);
         
         std::string stringify();
 
